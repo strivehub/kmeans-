4994a8ce40198517cd9ab5a637d7ce3b5bc7ad27
strivehub <49354455+strivehub@users.noreply.github.com>
2019-06-01 20:44:02 +0800

    Update k-means.py
    添加描述

--- k-means.py
+++ k-means.py
@@ -2,26 +2,26 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_table("kmeans_data.txt",header=None,names=("x",'y'))
+data = pd.read_table("kmeans_data.txt",header=None,names=("x",'y'))  #读取数据
 x = data['x']
 y = data['y']
 
-def centers(k):
+def centers(k):  #随机产生中心的x,y坐标
     center = np.random.choice(np.arange(-5,5,0.1),size=[k,2])
     return center
 
-def distence(data,center):
+def distence(data,center):  #计算每一个样本与K个中心坐标的欧氏距离，所以返回的是：样本数*坐标数维矩阵
     dis = np.zeros(shape=[data.shape[0],center.shape[0]])
     for i in range(len(data)):
         for j in range(len(center)):
             dis[i][j] = np.sqrt(np.sum(np.square(data.iloc[i]-center[j])))
     return dis
 
-def near_cen(dis):
+def near_cen(dis):   #返回每行与中心距离最近的索引
     result = np.argmin(dis,axis=1)
     return result
 
-def kmeans(data,k):
+def kmeans(data,k):  #更新中心点坐标
     center = centers(k)
     for i in range(10):
         dis = distence(data, center)
